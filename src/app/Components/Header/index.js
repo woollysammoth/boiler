0a8718488672295cba21f5cc84fe2a47628863b7
Sam <amgenstudios@gmail.com>
2018-02-06 20:33:07 -0500

    Tests

--- src/app/Components/Header/index.js
+++ src/app/Components/Header/index.js
@@ -8,7 +8,7 @@ export default class Header extends Component {
 			<header class={style.header}>
 				<h1>Preact Boilerplate</h1>
 				<nav>
-					<Link activeClassName={style.active} href="/">Home {style}</Link>
+					<Link activeClassName={style.active} href="/">Home</Link>
 					<Link activeClassName={style.active} href="/profile">Me</Link>
 					<Link activeClassName={style.active} href="/profile/test">Test</Link>
 				</nav>
